76d20354d3a2ad036ac467005dca8e888a64cc93
GravityWolfNotAmused <gravitywolfnotamused@gmail.com>
2018-11-05 05:45:00 -0500

    Update
    Updated weather values to never have rain, or fog.
    
    Provided comments for all created functions for a better understanding of what I  am doing in each.
    
    Changed addMag to take a TStringArray for all mags instead of seperate strings for different mags. Also, mags will be added to your hot bar, 3 and 4 for primary, and 5 and 6 for secondary. Please make sure that you have enough slots in your gear to take those mags.
    
    Removal of many EntityAI objects, didn't need to store them unless I needed to access them later.
    
    Creation of battery check, the TStringArray for batteryCompatibility is incomplete, and could possible have more attacchments. More will be added as they are brought to my attention, or they are released.
    
    Creation of arm_player_secondary, this allows the pistol/secondary to be added to the inventory rather than adding it to the hands.
    
    Return type for arm_player/arm_player_secondary changed from void to EntityAI, so I could add the weapons to QuickBar later.
    
    Added QuickBar settings for primary, and secondary.

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ## Please feel free to ask questions, or post updates. I am trying to get things as simply written as possible.
 
 ### Config:
-####   In order for you to change the classes, change the string which are stored TStringArrays, and strings to match the items you wish for the class to spawn with.
+####   In order for you to change the classes, change the string which are stored TStringArrays, and strings variables to match the items you wish for the class to spawn with.
 
 ### To Change Spawn Item Values:
 1. Change the values between the "" to the names of items which you wish to replace.
@@ -15,18 +15,22 @@
 (Before Change)
       
 ```c
-  autoptr TStringArray Assault_Clothing = {"BandanaMask_CamoPattern", "BallisticHelmet_Green", "USMCJacket_Woodland", "USMCPants_Woodland"};
-  string Assault_Gun = "M4A1";
-  autoptr TStringArray Assault_Attachments = {"M4_CQBBttstck_Black", "ACOGOptic", "M4_RISHndgrd_Black"};
-  string Assault_Mag = "MAG_STANAG_30Rnd";
+	autoptr TStringArray Assault_Clothing = {"BandanaMask_CamoPattern", "BallisticHelmet_Green", "USMCJacket_Woodland", "USMCPants_Woodland"};
+    string Assault_Gun = "M4A1";
+    string Assault_SideArm = "MakarovIJ70";
+    autoptr TStringArray Assault_SideArm_Attachments = {"MakarovPBSuppressor"};
+    autoptr TStringArray Assault_Attachments = {"M4_CQBBttstck_Black", "ACOGOptic", "M4_RISHndgrd_Black"};
+    autoptr TStringArray Assault_Mags = {"MAG_STANAG_30Rnd", "MAG_IJ70_8RND"};
 ```
       
 (After change) 
 ```c
-  autoptr TStringArray Assault_Clothing = {"BandanaMask_CamoPattern", "BallisticHelmet_Green", "USMCJacket_Woodland", "USMCPants_Woodland"};
-  string Assault_Gun = "AK74";
-  autoptr TStringArray Assault_Attachments = {"AK74_Hndgrd", "AK74_WoodBttstck"};
-  string Assault_Mag = "Mag_AK74_30Rnd";
+	autoptr TStringArray Assault_Clothing = {"BandanaMask_CamoPattern", "BallisticHelmet_Green", "USMCJacket_Woodland", "USMCPants_Woodland"};
+    string Assault_Gun = "M4A1";
+    string Assault_SideArm = "MakarovIJ70";
+    autoptr TStringArray Assault_SideArm_Attachments = {"MakarovPBSuppressor"};
+    autoptr TStringArray Assault_Attachments = {"AK_Woodbttstck", "AK_RailHndgrd" };
+    autoptr TStringArray Assault_Mags = {"Mag_AKM_30Rnd", "MAG_IJ70_8RND"};
 ```
 
 3. ...
@@ -36,10 +40,12 @@
 1. In order to create a new type of class, you must create the same basic layout of TStringArrays, and strings; however, you must name the variable something different than the other classes.
 
 ```c
-  autoptr TStringArray new_class = {"BandanaMask_CamoPattern", "BallisticHelmet_Green", "USMCJacket_Woodland", "USMCPants_Woodland"};
-  string new_class_gun = "AK74";
-  autoptr TStringArray new_class_gun_attachments = {"AK74_Hndgrd", "AK74_WoodBttstck"};
-  string new_class_mag = "Mag_AK74_30Rnd";
+	autoptr TStringArray new_class_clothes = {"BandanaMask_BlackPattern", "USMCJacket_Desert", "USMCPants_Desert", "DarkMotoHelmet_Black"};
+	string new_class_gun = "M4A1";
+    string new_class_sidearm = "MakarovIJ70";
+    autoptr TStringArray new_class_gun_sidearm_attachments = {"MakarovPBSuppressor"};
+    autoptr TStringArray new_class_gun_attachments = {"M4_CQBBttstck_Black", "ACOGOptic", "M4_RISHndgrd_Black"};
+    autoptr TStringArray new_class_mags = {"MAG_STANAG_30Rnd", "MAG_IJ70_8RND"};
 ```
 
 2. You must add an additional case to the switch statement, change the 4 in Math.RandomInt(0,4) to how many classes you have in total.
@@ -48,71 +54,83 @@
 
   (Before Additional Class)
 ```c
-switch (Math.RandomInt(0, 4)) {
-        case 0:
-          dress_player(player, Assault_Clothing, Uniform);
-          arm_player(player, Assault_Gun, Assault_Attachments);
-          give_utilities(player, Utilities, bag_name, Assault_Mag);
-          meleeWeapon = player.GetInventory().CreateInInventory(oneHanders.GetRandomElement());
-          break;
-        case 1:
-          dress_player(player, Sniper_Clothing, Uniform);
-          arm_player(player, Sniper_Gun, Sniper_Attachments);
-          give_utilities(player, Utilities, bag_name, Sniper_Mag);
-          meleeWeapon = player.GetInventory().CreateInInventory(oneHanders.GetRandomElement());
-          break;
-        case 2:
-          dress_player(player, Submachine_Clothing, Uniform);
-          arm_player(player, SubMachineGun_Gun, SubmachineGun_Attachments);
-          give_utilities(player, Utilities, bag_name, SubmachineGun_Mag);
-          meleeWeapon = player.GetInventory().CreateInInventory(oneHanders.GetRandomElement());
-          break;
-        case 3:
-          dress_player(player, Medic_Clothing, Uniform);
-          arm_player(player, Medic_Gun, Medic_Attachments);
-          give_utilities(player, Utilities, bag_name, Medic_Mag);
-          meleeWeapon = player.GetInventory().CreateInInventory(oneHanders.GetRandomElement());
-          break;
-      }
+	switch (Math.RandomInt(0, 4)) {
+		case 0: // Assault
+			dress_player(player, Assault_Clothing, Uniform);
+			primary = arm_player(player, Assault_Gun, Assault_Attachments);
+			secondary = arm_player_secondary(player, Assault_SideArm, Assault_SideArm_Attachments);
+			give_utilities(player, Utilities, bag_name);
+			addMags(player, Assault_Mags, magCount, true);
+			break;
+		case 1: // Sniper
+			dress_player(player, Sniper_Clothing, Uniform);
+			primary = arm_player(player, Sniper_Gun, Sniper_Attachments);
+			secondary = arm_player_secondary(player, Sniper_Sidearm, Sniper_SideArm_Attachments);
+			give_utilities(player, Utilities, bag_name);
+			addMags(player, Sniper_Mags, magCount, true);
+			break;
+		case 2: // Submachine Gun
+			dress_player(player, Submachine_Clothing, Uniform);
+			primary = arm_player(player, SubMachineGun_Gun, SubmachineGun_Attachments);
+			secondary = arm_player_secondary(player, SubMachineGun_Sidearm, SubMachineGun_SideArm_Attachments);
+			give_utilities(player, Utilities, bag_name);
+			addMags(player, SubmachineGun_Mags, magCount, true);
+			break;
+		case 3: // Medic
+			dress_player(player, Medic_Clothing, Uniform);
+			primary = arm_player(player, Medic_Gun, Medic_Attachments);
+			give_utilities(player, Utilities, bag_name);
+			addMags(player, Medic_Mags, magCount, true);
+			give_medical(player, Medical_Supply_Case, Medical_Supplies);
+			break;
+	}
 ```
 
 (After Additional Class)
 ```c
-      switch (Math.RandomInt(0, 5) {
-        case 0:
-          dress_player(player, Assault_Clothing, Uniform);
-          arm_player(player, Assault_Gun, Assault_Attachments);
-          give_utilities(player, Utilities, bag_name, Assault_Mag);
-          meleeWeapon = player.GetInventory().CreateInInventory(oneHanders.GetRandomElement());
-          break;
-        case 1:
-          dress_player(player, Sniper_Clothing, Uniform);
-          arm_player(player, Sniper_Gun, Sniper_Attachments);
-          give_utilities(player, Utilities, bag_name, Sniper_Mag);
-          meleeWeapon = player.GetInventory().CreateInInventory(oneHanders.GetRandomElement());
-          break;
-        case 2:
-          dress_player(player, Submachine_Clothing, Uniform);
-          arm_player(player, SubMachineGun_Gun, SubmachineGun_Attachments);
-          give_utilities(player, Utilities, bag_name, SubmachineGun_Mag);
-          meleeWeapon = player.GetInventory().CreateInInventory(oneHanders.GetRandomElement());
-          break;
-        case 3:
-          dress_player(player, Medic_Clothing, Uniform);
-          arm_player(player, Medic_Gun, Medic_Attachments);
-          give_utilities(player, Utilities, bag_name, Medic_Mag);
-          meleeWeapon = player.GetInventory().CreateInInventory(oneHanders.GetRandomElement());
-          break;
-        case 4:
-          dress_player(player, new_class_clothing, Uniform);
-          arm_player(player, new_class_gun, new_class_gun_attachments);
-          give_utilities(player, Utilities, bag_name, new_class_mag);
-          meleeWeapon = player.GetInventory().CreateInInventory(oneHanders.GetRandomElement());
-          break;
-      }
+	//Notice below: Math.RandomInt has change from 0,4 to 0,5. the last number should always be the amount of classes you have in total.
+	switch (Math.RandomInt(0, 5)) {
+		case 0: // Assault
+			dress_player(player, Assault_Clothing, Uniform);
+			primary = arm_player(player, Assault_Gun, Assault_Attachments);
+			secondary = arm_player_secondary(player, Assault_SideArm, Assault_SideArm_Attachments);
+			give_utilities(player, Utilities, bag_name);
+			addMags(player, Assault_Mags, magCount, true);
+			break;
+		case 1: // Sniper
+			dress_player(player, Sniper_Clothing, Uniform);
+			primary = arm_player(player, Sniper_Gun, Sniper_Attachments);
+			secondary = arm_player_secondary(player, Sniper_Sidearm, Sniper_SideArm_Attachments);
+			give_utilities(player, Utilities, bag_name);
+			addMags(player, Sniper_Mags, magCount, true);
+			break;
+		case 2: // Submachine Gun
+			dress_player(player, Submachine_Clothing, Uniform);
+			primary = arm_player(player, SubMachineGun_Gun, SubmachineGun_Attachments);
+			secondary = arm_player_secondary(player, SubMachineGun_Sidearm, SubMachineGun_SideArm_Attachments);
+			give_utilities(player, Utilities, bag_name);
+			addMags(player, SubmachineGun_Mags, magCount, true);
+			break;
+		case 3: // Medic
+			dress_player(player, Medic_Clothing, Uniform);
+			primary = arm_player(player, Medic_Gun, Medic_Attachments);
+			give_utilities(player, Utilities, bag_name);
+			addMags(player, Medic_Mags, magCount, true);
+			give_medical(player, Medical_Supply_Case, Medical_Supplies);
+			break;
+		case 4: //New Class
+			dress_player(player, new_class_clothes, Uniform);
+			primary = new_class_gun(player, SubMachineGun_Gun, new_class_gun_attachments);
+			secondary = arm_player_secondary(player, new_class_sidearm, new_class_gun_sidearm_attachments);
+			give_utilities(player, Utilities, bag_name);
+			addMags(player, new_class_mags, magCount, true);
+			break;
+	}
  ```
 
 ### Reminder:
   Some items, which maybe provided by Item name lists, may not be completely integrated yet. I've had this issue testing the M249, and one AK, I believe it was the AK101; however, this could changed with DayZ Updates.
+
+Special thanks to u/ThisAccountIs4Reddit for putting together a semi-complete item list: https://docs.google.com/spreadsheets/d/1jFQOkaU6kSZJFzMV4NHHgmi95htJzS0_1iM8aCWYCvI/edit?usp=sharing
 
 # DO NOT FORGET TO CHANGE YOUR SPAWN POINTS TO A SINGLE TOWN/AREA INSIDE OF cfgplayerspawnpoints.xml
--- init.c
+++ init.c
@@ -1,4 +1,3 @@
-
 void main() {
 
     Hive ce = CreateHive();
@@ -7,122 +6,214 @@ void main() {
 
     Weather weather = g_Game.GetWeather();
 
-    weather.GetOvercast().SetLimits(0.0, 1.0);
-    weather.GetRain().SetLimits(0.0, 1.0);
-    weather.GetFog().SetLimits(0.0, 0.25);
+    weather.GetOvercast().SetLimits(0.0, 0.0);
+    weather.GetRain().SetLimits(0.0, 0.0);
+    weather.GetFog().SetLimits(0.0, 0.0);
 
-    weather.GetOvercast().SetForecastChangeLimits(0.0, 0.2);
-    weather.GetRain().SetForecastChangeLimits(0.0, 0.1);
-    weather.GetFog().SetForecastChangeLimits(0.15, 0.45);
+    weather.GetOvercast().SetForecastChangeLimits(0.0, 0.0);
+    weather.GetRain().SetForecastChangeLimits(0.0, 0.0);
+    weather.GetFog().SetForecastChangeLimits(0.0, 0.0);
 
-    weather.GetOvercast().SetForecastTimeLimits(1800, 1800);
-    weather.GetRain().SetForecastTimeLimits(600, 600);
-    weather.GetFog().SetForecastTimeLimits(1800, 1800);
+    weather.GetOvercast().SetForecastTimeLimits(0, 0);
+    weather.GetRain().SetForecastTimeLimits(0,0);
+    weather.GetFog().SetForecastTimeLimits(0,0);
 
     weather.GetOvercast().Set(Math.RandomFloatInclusive(0.0, 0.3), 0, 0);
     weather.GetRain().Set(Math.RandomFloatInclusive(0.0, 0.2), 0, 0);
     weather.GetFog().Set(Math.RandomFloatInclusive(0.0, 0.1), 0, 0);
 
-    weather.SetWindMaximumSpeed(15);
-    weather.SetWindFunctionParams(0.1, 0.3, 50);
+    weather.SetWindMaximumSpeed(0);
+    weather.SetWindFunctionParams(0, 0, 0);
 }
 
 class CustomMission : MissionServer {
+    /*
+     *  Config:
+     *      This area is the only area in which users should change any information/variables.
+     *      If you do not understand what something is being used for please read the comments above every function.
+    */
+
+    //Mag Count
     int magCount = 2;
-    /*
-     *  Bag Name
-     */
+
+    //Bag Name
     string bag_name = "SmershBag";
+
     /*
-     *  Assault Class
-     */
+     * Name: Assault Class
+     * Primary Weapon: M4A1
+     * Weapon Attachments: CQB Buttstock, ACOG Scope, RIS Handguard
+     * Sidearm Weapon: IJ70
+     * Sidearm Attachments: Suppressor
+    */
     autoptr TStringArray Assault_Clothing = {"BandanaMask_CamoPattern", "BallisticHelmet_Green", "USMCJacket_Woodland", "USMCPants_Woodland"};
     string Assault_Gun = "M4A1";
     string Assault_SideArm = "MakarovIJ70";
     autoptr TStringArray Assault_SideArm_Attachments = {"MakarovPBSuppressor"};
     autoptr TStringArray Assault_Attachments = {"M4_CQBBttstck_Black", "ACOGOptic", "M4_RISHndgrd_Black"};
-    string Assault_Mag = "MAG_STANAG_30Rnd";
+    autoptr TStringArray Assault_Mags = {"MAG_STANAG_30Rnd", "MAG_IJ70_8RND"}
 
     /*
-     *  Submachine Gun Class
-     */
+     * Name: Submachine Gun Class
+     * Primary Weapon: MP5K
+     * Weapon Attachments: Rail Handguard, Buttstock, M69 Optic
+     * Sidearm Weapon: IJ70
+     * Sidearm Attachments: Suppressor
+    */
     autoptr TStringArray Submachine_Clothing = {"M65Jacket_Black", "SlacksPants_Black", "RocketAviators", "BalaclavaMask_Blackskull"};
     string SubMachineGun_Gun = "MP5K";
-    autoptr TStringArray SubmachineGun_Attachments = {"MP5_PRailHndgrd", "MP5k_StockBttstck"};
-    string SubmachineGun_Mag = "MAG_MP5_15Rnd";
+    string SubMachineGun_Sidearm = "MakarovIJ70";
+    autoptr TStringArray SubMachineGun_SideArm_Attachments = {"MakarovPBSuppressor"};
+    autoptr TStringArray SubmachineGun_Attachments = {"MP5_PRailHndgrd", "MP5k_StockBttstck", "M68Optic"};
+    autoptr TStringArray SubmachineGun_Mags = {"MAG_MP5_15Rnd", "MAG_IJ70_8RND"};
 
     /*
-     *  Sniper Class
-     */
-
-    autoptr TStringArray Sniper_Clothing = {"GhillieHood_Mossy", "GhillieSuit_Mossy", "GhillieTop_Mossy"};
-    string Sniper_Gun = "MOSIN9130_CAMO";
-    autoptr TStringArray Sniper_Attachments = {"PUScopeOptic"};
-    string Sniper_Mag = "Mag_CLIP762x54_5Rnd";
+     * Name: Sniper Class
+     * Primary Weapon: Mosin
+     * Weapon Attachments: PU Scope
+     * Sidearm Weapon: IJ70
+     * Sidearm Attachments: Suppressor
+    */
+    autoptr TStringArray Sniper_Clothing = {"BalaclavaMask_Black", "BallisticHelmet_Black", "TShirt_Black", "Jeans_Black", "UKAssVest_Black"};
+    string Sniper_Gun = "SVD";
+    string Sniper_Sidearm = "MakarovIJ70";
+    autoptr TStringArray Sniper_SideArm_Attachments = {"MakarovPBSuppressor"};
+    autoptr TStringArray Sniper_Attachments = {"PSO11Optic"};
+    autoptr TStringArray Sniper_Mags = {"Mag_SVD_10Rnd", "MAG_IJ70_8RND"};
 
     /*
-     *  Medic Class
+      * Name: Medic Class
+      * Primary Weapon: None
+      * Weapon Attachments: None
+      * Sidearm Weapon: IJ70
+      * Sidearm Attachments: Suppressor
+      * Medical Kit: Bandage, Morphine, Saline, Painkillers
      */
     autoptr TStringArray Medic_Clothing = {"BallisticHelmet_Green", "GasMask", "ParamedicJacket_Green", "ParamedicPants_Green"};
     string Medic_Gun = "MakarovIJ70";
     autoptr TStringArray Medic_Attachments = {"MakarovPBSuppressor"};
     string Medical_Supply_Case = "FIRSTAIDKIT";
     autoptr TStringArray Medical_Supplies = {"BANDAGEDRESSING", "MORPHINE", "SALINEBAGIV", "PAINKILLERTABLETS"};
-    string Medic_Mag = "MAG_IJ70_8RND";
+    autoptr TStringArray Medic_Mags = {"MAG_IJ70_8RND"};
 
-    /*
-     *  General Items which everyone will receive on spawn; regardless of class.
-     */
+    //  General Items which everyone will receive on spawn; regardless of class.
     autoptr TStringArray Utilities = {"TacticalBaconCan", "Canteen", "CanOpener"};
     autoptr TStringArray Uniform = {"SmershVest", "TacticalGloves_Green", "CombatBoots_Black"};
 
-    /*
-     *  Melee Weapons
-     */
+    // Melee Weapons
     autoptr TStringArray oneHanders = {"Machete", "HuntingKnife", "KitchenKnife"};
 
-    void addMags(PlayerBase player, string mag_type, int count) {
-        if (count < 1)
+    /*
+     * End of Config:
+     *   Do not change anything below unless you understand what you are doing....
+    */
+
+    autoptr TStringArray batteryCompatible = {"UniversalLight", "ReflexOptic", "M68Optic", "M4_T3NRDSOptic"};
+
+    /*
+     * @function addMags
+     * @discussion Adds mags to players inventory
+     *
+     * @param    player: Instance Of Player
+     * @param    magArray: Collection mag names
+     * @param    count: Number of each mag which needs to be given.
+     * @param    toQuickBar: Should be added to the quick bar, or not.
+     */
+    void addMags(PlayerBase player, TStringArray magArray, int count, bool toQuickBar) {
+        if (count < 1 || magArray.Count() < 1)
             return;
 
-        if(mag_type.Length() == 0) {
-            return;
-        }
-
-        EntityAI mag;
-
-        for (int i = 0; i < count; i++) {
-            mag = player.GetInventory().CreateInInventory(mag_type);
+        for(int x = 0; x < magArray.Count(); x++) {
+            for (int i = 0; i < count; i++) {
+                EntityAI mag = player.GetInventory().CreateInInventory(magArray.Get(x));
+                if (toQuickBar)
+                    if(x != 1) {
+                        player.SetQuickBarEntityShortcut(mag, x + i + 2, true);
+                    }else{
+                        player.SetQuickBarEntityShortcut(mag, x + i + 3, true);
+                    }
+            }
         }
     }
+
+    /*
+     * @function dress_player
+     * @abstract Gives player class related clothing.
+     * @discussion This function puts choosen class clothing on the player.
+     *
+     * @param    player: Instance Of Player
+     * @param    class_clothes: Collection of class clothing names
+     * @param    general_items: Collection of general items which will also be given to the player
+     */
     void dress_player(PlayerBase player, TStringArray class_clothes, TStringArray general_items){
-        EntityAI wearable;
-        for(int i = 0; i < class_clothes.Count(); i++){
-            wearable = player.GetInventory().CreateAttachment(class_clothes.Get(i));
-        }
-        for(int x = 0; x < general_items.Count(); x++){
-            wearable = player.GetInventory().CreateAttachment(general_items.Get(x));
-        }
+        for(int i = 0; i < class_clothes.Count(); i++)
+            player.GetInventory().CreateAttachment(class_clothes.Get(i));
+
+        for(int x = 0; x < general_items.Count(); x++)
+            player.GetInventory().CreateAttachment(general_items.Get(x));
     }
-    void arm_player(PlayerBase player, string weapon_name, TStringArray gunAttachments){
+    
+    /*
+     *  @function   arm_player
+     *  @abstract   Arms player with weapon, and adds attachments to weapon.
+     *  @discussion This function creates a weapon, goes through all the attachments and attaches them,
+     *                while checking if it needs a battery.
+     *  @param      player: Instance of Player
+     *  @param      weapon_name: Name of weapon to be given
+     *  @param      gunAttachments: Collection of attachment names to be added to weapon
+     *  @result     EntityAI: weapon
+     */
+    EntityAI arm_player(PlayerBase player, string weapon_name, TStringArray gunAttachments){
         EntityAI weapon = player.GetHumanInventory().CreateInHands(weapon_name);
         for(int i = 0; i < gunAttachments.Count(); i++){
             EntityAI attachment = weapon.GetInventory().CreateAttachment(gunAttachments.Get(i));
+            for (int x = 0; x < batteryCompatible.Count(); x++){
+                if(batteryCompatible.Get(x).Contains(gunAttachments.Get(i)))
+                    attachment.GetInventory().CreateAttachment("Battery9V");
+            }
         }
+        return weapon;
     }
-    void give_utilities(PlayerBase player, TStringArray util, string backpack, string mag){
+
+    EntityAI arm_player_secondary(PlayerBase player, string weapon_name, TStringArray secondaryAttachments){
+        EntityAI weapon = player.GetInventory().CreateInInventory(weapon_name);
+        for(int i = 0; i < secondaryAttachments.Count(); i++){
+            EntityAI attachment = weapon.GetInventory().CreateAttachment(secondaryAttachments.Get(i));
+            for (int x = 0; x < batteryCompatible.Count(); x++){
+                if(batteryCompatible.Get(x).Contains(secondaryAttachments.Get(i)))
+                    attachment.GetInventory().CreateAttachment("Battery9V");
+            }
+        }
+        return weapon;
+    }
+
+    /*
+     *  @function   give_utilities
+     *  @discussion Added a backpack to the players back and fills it with provided items.
+     *
+     *  @param      player: Instance of Player
+     *  @param      util: Collection of utility items names to be added to the backpack
+     *  @param      backpack: Backpack item name which items will be stored, and will be added to players back.
+     */
+    void give_utilities(PlayerBase player, TStringArray util, string backpack){
         EntityAI backpackEnt = player.GetInventory().CreateAttachment(backpack);
-        for (int i = 0; i < util.Count(); i++) {
-            EntityAI contents = backpackEnt.GetInventory().CreateInInventory(util.Get(i));
-        }
-        addMags(player, mag, magCount);
+        for (int i = 0; i < util.Count(); i++)
+            backpackEnt.GetInventory().CreateInInventory(util.Get(i));
     }
+
+    /*
+    *  @function   give_medical
+    *  @discussion Puts a bag into players inventory and adds items into the bag.
+    *                  This is NOT the same as give_utilities, one adds bag to inventory, one adds bag to player.
+    *  @param      player: Instance of Player
+    *  @param      bag_name: Name of the bag which will be added to the players inventory and filled with items.
+    *  @param      supplies: Collection of items which will be added to the bag.
+    */
     void give_medical(PlayerBase player, string bag_name, TStringArray supplies) {
         EntityAI medickit = player.GetInventory().CreateInInventory(bag_name);
 
-        for (int i = 0; i < supplies.Count(); i++) {
+        for (int i = 0; i < supplies.Count(); i++)
             medickit.GetInventory().CreateInInventory(supplies.Get(i));
-        }
     }
 
     override PlayerBase CreateCharacter(PlayerIdentity identity, vector pos, ParamsReadContext ctx, string characterName){
@@ -138,33 +229,44 @@ class CustomMission : MissionServer {
     override void StartingEquipSetup(PlayerBase player, bool clothesChosen) {
         player.RemoveAllItems();
 
-        EntityAI meleeWeapon;
+        EntityAI primary;
+        EntityAI secondary;
 
         switch (Math.RandomInt(0, 4)) {
-            case 0:
+            case 0: // Assault
                 dress_player(player, Assault_Clothing, Uniform);
-                arm_player(player, Assault_Gun, Assault_Attachments);
-                give_utilities(player, Utilities, bag_name, Assault_Mag);
-                meleeWeapon = player.GetInventory().CreateInInventory(oneHanders.GetRandomElement());
+                primary = arm_player(player, Assault_Gun, Assault_Attachments);
+                secondary = arm_player_secondary(player, Assault_SideArm, Assault_SideArm_Attachments);
+                give_utilities(player, Utilities, bag_name);
+                addMags(player, Assault_Mags, magCount, true);
                 break;
-            case 1:
+            case 1: // Sniper
                 dress_player(player, Sniper_Clothing, Uniform);
-                arm_player(player, Sniper_Gun, Sniper_Attachments);
-                give_utilities(player, Utilities, bag_name, Sniper_Mag);
-                meleeWeapon = player.GetInventory().CreateInInventory(oneHanders.GetRandomElement());
+                primary = arm_player(player, Sniper_Gun, Sniper_Attachments);
+                secondary = arm_player_secondary(player, Sniper_Sidearm, Sniper_SideArm_Attachments);
+                give_utilities(player, Utilities, bag_name);
+                addMags(player, Sniper_Mags, magCount, true);
                 break;
-            case 2:
+            case 2: // Submachine Gun
                 dress_player(player, Submachine_Clothing, Uniform);
-                arm_player(player, SubMachineGun_Gun, SubmachineGun_Attachments);
-                give_utilities(player, Utilities, bag_name, SubmachineGun_Mag);
-                meleeWeapon = player.GetInventory().CreateInInventory(oneHanders.GetRandomElement());
+                primary = arm_player(player, SubMachineGun_Gun, SubmachineGun_Attachments);
+                secondary = arm_player_secondary(player, SubMachineGun_Sidearm, SubMachineGun_SideArm_Attachments);
+                give_utilities(player, Utilities, bag_name);
+                addMags(player, SubmachineGun_Mags, magCount, true);
                 break;
-            case 3:
+            case 3: // Medic
                 dress_player(player, Medic_Clothing, Uniform);
-                arm_player(player, Medic_Gun, Medic_Attachments);
-                give_utilities(player, Utilities, bag_name, Medic_Mag);
-                meleeWeapon = player.GetInventory().CreateInInventory(oneHanders.GetRandomElement());
+                primary = arm_player(player, Medic_Gun, Medic_Attachments);
+                give_utilities(player, Utilities, bag_name);
+                addMags(player, Medic_Mags, magCount, true);
+                give_medical(player, Medical_Supply_Case, Medical_Supplies);
                 break;
+        }
+        player.GetInventory().CreateInInventory(oneHanders.GetRandomElement());
+        player.SetQuickBarEntityShortcut(primary, 0, true);
+
+        if(secondary != null){
+            player.SetQuickBarEntityShortcut(secondary, 1, true);
         }
     }
 };
